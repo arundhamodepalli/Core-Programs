99b21c3ededc349fa3ece6617a6c832e413a3959
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-02-24 15:24:27 +0530

    percentage of head vs tails

---
+++ Core_Programs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Core_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to basic core programs");
+        }
+    }
+}
